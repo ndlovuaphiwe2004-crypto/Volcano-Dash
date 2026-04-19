1acb1550e5811b11706089aa050aadd19241f6f2
Motlatsontjana831-bit <Motlatsontjana831@gmail.com>
2026-04-19 12:16:58 +0200

    Unity Update

---
+++ Assets/UI/PlayerHealth.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health, maxHealth = 10.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
