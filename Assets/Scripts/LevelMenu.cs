ae19ff1ceaeaa4e7be0f9cd4f9580988bb4c247b
aphiwe210 <ndlovu.aphiwe.2004@gmail.com>
2026-06-01 06:48:28 +0200

    Update LevelMenu.cs

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -9,23 +9,36 @@ public class LevelMenu : MonoBehaviour
     public Button[] buttons;
     public GameObject levelButtons;
 
-    // In LevelMenu.cs
     private void Awake()
     {
         ButtonsToArray();
+
         int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+
         for (int i = 0; i < buttons.Length; i++)
         {
             buttons[i].interactable = false;
         }
 
-        for (int i = 0; i < unlockedLevel; i++)
+        int levelsToUnlock = Mathf.Min(unlockedLevel, buttons.Length);
+        for (int i = 0; i < levelsToUnlock; i++)
         {
             buttons[i].interactable = true;
         }
     }
+
     public void OpenLevel(int levelId)
     {
+        int currentSavedHighest = PlayerPrefs.GetInt("UnlockedLevel", 1);
+
+        int nextLevel = levelId + 1;
+
+        if (nextLevel > currentSavedHighest)
+        {
+            PlayerPrefs.SetInt("UnlockedLevel", nextLevel);
+            PlayerPrefs.Save();
+        }
+
         string levelName = "Level " + levelId;
         SceneManager.LoadScene(levelName);
     }
@@ -38,5 +51,11 @@ public class LevelMenu : MonoBehaviour
         {
             buttons[i] = levelButtons.transform.GetChild(i).gameObject.GetComponent<Button>();
         }
+    }
+
+    public void ResetProgress()
+    {
+        PlayerPrefs.DeleteKey("UnlockedLevel");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
